2d06e9f4856501f4dca7676d86638320ec99fd66
KIKIWHYY <yougonnadie123@mail.ru>
2021-12-30 01:25:45 +0400

    logo fix, buggs with height of html, body, wrapper blocks

--- modules/statistics.js
+++ modules/statistics.js
@@ -1,9 +1,21 @@
+/* const { MessageAttachment } = require('discord.js')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const Canvas = require('canvas');
+const fetch = require('node-fetch');
+const config = require('../config.json');
+const GIFEncoder = require('gif-encoder-2');
+const gifFrames = require('gif-frames');
+let global_client;
+Canvas.registerFont('./source/fonts/Comfortaa-Bold.ttf', { family: 'Comfortaa' }); */
+
 function Get_Statistics(client)
 {
     try {
-        console.log(client);
-        // const GUILDS = client.guilds.cache.map(p => console.log(p));
-        // console.log(GUILDS);
+        // ! Можно просто передать кэш, либо создать из них map ?
+        const GUILDS_ID = client.guilds.cache.map(g => g.id);
+        console.log(GUILDS_ID);
     }
     catch (err) {
         console.log(err);
