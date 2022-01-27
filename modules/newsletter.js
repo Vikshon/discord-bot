97451311cc243853d91f5ced325be7fcb03390e7
KIKIWHYY <yougonnadie123@mail.ru>
2022-01-27 20:17:17 +0400

    comparing urls in newsletter.js

--- modules/newsletter.js
+++ modules/newsletter.js
@@ -18,7 +18,7 @@ async function Get_Post(client) {
             let last_post = items[0];
 
             let fl = 1;
-            fl = await Compare_Post(last_post, client, CURRENT_GUILD);
+            fl = await Compare_Post(last_post, client, GROUP, CURRENT_GUILD);
             if (fl == 0)
                 return false;
             let attachments = await Get_Attachments(last_post);
@@ -27,7 +27,7 @@ async function Get_Post(client) {
     }
 }
 
-async function Compare_Post(last_post, client, CURRENT_GUILD) {
+async function Compare_Post(last_post, client, GROUP, CURRENT_GUILD) {
     let messages = await client.channels.cache.get(CURRENT_GUILD.channels.news).messages.fetch({ limit: 15 });
     let post_text = last_post.text;
     let isAds = last_post.marked_as_ads;
@@ -35,6 +35,8 @@ async function Compare_Post(last_post, client, CURRENT_GUILD) {
     let post_date = last_post.date * 1000;
     let current_date = Date.now();
     let hour_difference = Math.floor((current_date - post_date) / (1000 * 3600));
+    // ! Зарефакторить, просто добавить ссылку в объект при парсинге -->
+    let last_post_url = `https://vk.com/${GROUP}?w=wall${last_post.owner_id}_${last_post.id}`;
 
     let fl = 1;
     if (isAds || edited || hour_difference > 1)
@@ -43,8 +45,10 @@ async function Compare_Post(last_post, client, CURRENT_GUILD) {
     // ! Может убрать эту проверку текста сообщений? Оставить просто проверку даты. Если сообщение старее часа, например, то и пропускаем его
     // ! ИЛИ можно проверять просто по ссылке на пост. Это так-то нормальная идея. Просто всегда её можно скидывать, типа ссылка на источник
     for (let msg of messages) {
+        // ! Находим первую ссылку сообщения, что в новом формате будет ссылка на пост группы. Далее проверям ее с ссылкой на источник, если совпали, пост пропускаем
+        let msg_post_url = msg[1].content.match(/https:\/\/.+/)[0];
         // console.log('[DEBUG] Comparing msg and post: ', msg[1].content == post_date || msg[1].content.startsWith(post_text))
-        if (msg[1].content == post_date || msg[1].content.startsWith(post_text)) {
+        if (msg[1].content == post_date || msg[1].content.startsWith(post_text) || msg_post_url == last_post_url) {
             fl = 0;
             break;
         }
@@ -105,9 +109,8 @@ async function Send_Post(last_post, attachments, GROUP, client, CURRENT_GUILD) {
         let last_post_url = `https://vk.com/${GROUP}?w=wall${last_post.owner_id}_${last_post.id}`;
 
         let msg = last_post.text || last_post.date * 1000;
-        let attachments_str = `\n\n● Ссылки:\n`;
-        if (msg.length + attachments.length > 1900) {
-            // ! Нужно проверить работу -->
+        let attachments_str = `\n\n● Ссылки:\n${last_post_url}\n`;
+        if (msg.length + last_post_url.length + attachments.length > 1900) {
             let space_index = msg.slice(0, 1800).lastIndexOf(' ');
             msg = msg.slice(0, space_index);
             msg += `...\nПродолжение в источнике: <${last_post_url}>`;
@@ -125,17 +128,14 @@ async function Send_Post(last_post, attachments, GROUP, client, CURRENT_GUILD) {
         else {
             for (let a of attachments)
                 attachments_str += `${a}\n`;
-            if (attachments_str.length > 20)
-                msg += `${attachments_str}`;
+            
+            msg += `${attachments_str}`;
 
             await client.channels.cache.get(CURRENT_GUILD.channels.news).send({ content: msg });
         }
     }
     catch (error) {
-        if (last_post.text.length > 2000)
-            console.log('Слишком длинный текст!');
-        else
-            console.log(error);
+        console.log(error);
     }
 }
 
